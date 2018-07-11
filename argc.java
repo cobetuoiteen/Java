0fde805e338538a18b8721fb54183f5138ec168f
cobetuoiteen <tienhuynhq5@gmail.com>
2018-07-11 17:48:55 +0700

    Thứ tư, 11 Tháng bảy năm 2018 17:48:55 +07

---
+++ argc.java
@@ -0,0 +1,8 @@
+class Argc{
+        public static void main(String[] thamso) {
+                for (String elem: thamso) {
+                        System.out.println(elem);
+                }
+        }
+}
+
